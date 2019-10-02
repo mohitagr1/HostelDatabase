05e5492ceae2aaef48123ef1675294df1d19e1c3
mohitferrari <mohitagarwalferrari@gmail.com>
2019-10-02 15:51:29 +0530

    My First commit with spring boot setup

---
+++ src/test/java/com/hostelDatabase/hostelDatabase/HostelDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hostelDatabase.hostelDatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HostelDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
